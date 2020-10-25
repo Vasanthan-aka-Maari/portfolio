cdbd911c324e73592942122f0170b9d2b89e81c2
Vasanthan <vasanth2754@gmail.com>
2020-10-25 18:47:23 +0530

    Updates

---
+++ precache-manifest.0ed63a3e87f63a534d954c7b56c7590d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e46cad6a17c27d41086caa6d42a8e5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b131af254b62661d54c6",
+    "url": "/portfolio/static/css/main.cc5133d3.chunk.css"
+  },
+  {
+    "revision": "176c2463173e86108698",
+    "url": "/portfolio/static/js/2.9e8a127c.chunk.js"
+  },
+  {
+    "revision": "0efabacf1d5a43cdae1da6fa187843ce",
+    "url": "/portfolio/static/js/2.9e8a127c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b131af254b62661d54c6",
+    "url": "/portfolio/static/js/main.5b1b3711.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "612dd23aaade3d4222d5e77d71e0be61",
+    "url": "/portfolio/static/media/Resume.612dd23a.pdf"
+  },
+  {
+    "revision": "97b0420ae5d6e198b215c2183dd74d56",
+    "url": "/portfolio/static/media/food.97b0420a.jpg"
+  },
+  {
+    "revision": "14e468c014409315fabcc0c603d6cf7a",
+    "url": "/portfolio/static/media/fortnite-app.14e468c0.jpg"
+  },
+  {
+    "revision": "e01492aef6f2178f5735a73731e33dd7",
+    "url": "/portfolio/static/media/landingpage.e01492ae.jpg"
+  },
+  {
+    "revision": "145241de96baa5e3f4e11519f37f7b27",
+    "url": "/portfolio/static/media/netflixlogo.145241de.jpg"
+  },
+  {
+    "revision": "69aebd111bbf62187918d78ab577d1b4",
+    "url": "/portfolio/static/media/todo.69aebd11.jpg"
+  },
+  {
+    "revision": "2f3af3c9ccd99c05be674c074b8f7c51",
+    "url": "/portfolio/static/media/weather.2f3af3c9.jpg"
+  }
+]);
