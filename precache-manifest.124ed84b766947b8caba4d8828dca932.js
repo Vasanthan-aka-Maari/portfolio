d86ac9abba8f1310dd6b6bba99d25b869ecb3178
Vasanthan <vasanth2754@gmail.com>
2020-11-11 19:45:01 +0530

    Updates

---
+++ precache-manifest.124ed84b766947b8caba4d8828dca932.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfe7605e0d4986691f128cdab343447",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a565cc8a7db2fbefd36e",
+    "url": "/portfolio/static/css/main.770374c7.chunk.css"
+  },
+  {
+    "revision": "dbc63de6489f51d4d502",
+    "url": "/portfolio/static/js/2.7aaac341.chunk.js"
+  },
+  {
+    "revision": "ae261471e0387de2c27b0b5cf60c4be5",
+    "url": "/portfolio/static/js/2.7aaac341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a565cc8a7db2fbefd36e",
+    "url": "/portfolio/static/js/main.aaed61d3.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "612dd23aaade3d4222d5e77d71e0be61",
+    "url": "/portfolio/static/media/Resume.612dd23a.pdf"
+  },
+  {
+    "revision": "c21933320032cfd003e08b2d2d422f08",
+    "url": "/portfolio/static/media/amazon.c2193332.jpg"
+  },
+  {
+    "revision": "f75b911726a125f7d4ff22b0f9470143",
+    "url": "/portfolio/static/media/booklist.f75b9117.jpg"
+  },
+  {
+    "revision": "c6935996a5e0db64fb5d1243f7740f83",
+    "url": "/portfolio/static/media/food.c6935996.jpg"
+  },
+  {
+    "revision": "b60a6ea4e3e17c57395c3aa43d308474",
+    "url": "/portfolio/static/media/fortnite-app.b60a6ea4.jpg"
+  },
+  {
+    "revision": "c0124fc2d49a5ee1e5ad18a1fdea2bc3",
+    "url": "/portfolio/static/media/landingpage.c0124fc2.jpg"
+  },
+  {
+    "revision": "a136794a0e035e7b53e1a77a64550297",
+    "url": "/portfolio/static/media/logo.a136794a.svg"
+  },
+  {
+    "revision": "8443c3f1d0e61d543e95cc0ca6cd3aa0",
+    "url": "/portfolio/static/media/netflixlogo.8443c3f1.jpg"
+  },
+  {
+    "revision": "dddf2eef4eb5ed098772d67b2464a199",
+    "url": "/portfolio/static/media/weather.dddf2eef.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.953123122085797e02e2418ed4f6f970.js"
+  "/portfolio/precache-manifest.124ed84b766947b8caba4d8828dca932.js"
 );
 
 self.addEventListener('message', (event) => {
